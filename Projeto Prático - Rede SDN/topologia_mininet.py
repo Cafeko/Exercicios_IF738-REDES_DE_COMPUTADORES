d587dd8d0a111420afed05a27552534ee3d981a5
Cafeko <caiofgs2329@gmail.com>
2026-04-29 19:45:25 -0300

    Fase 1 - Topologia Mininet

---
+++ Projeto Prático - Rede SDN/topologia_mininet.py
@@ -0,0 +1,23 @@
+from mininet.topo import Topo
+from mininet.link import TCLink
+
+class TopologiaRede (Topo):
+    def build(self):
+        # Hosts:
+        h1 = self.addHost("h1", ip="10.0.0.1/24")
+        h2 = self.addHost("h2", ip="10.0.0.2/24")
+        h3 = self.addHost("h3", ip="10.0.0.3/24")
+        h4 = self.addHost("h4", ip="10.0.0.4/24")
+
+        # Switches:
+        s1 = self.addSwitch("s1")
+        s2 = self.addSwitch("s2")
+
+        # Links:
+        self.addLink(s1, s2, cls=TCLink, bw=100)
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s2)
+        self.addLink(h4, s2)
+
+topos = {"Rede": TopologiaRede}
